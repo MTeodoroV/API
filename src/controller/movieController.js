3f86e62e4e1c639d911d1f0c4e5eb09785071901
Mateus <mateusteodorovieirabaptista@gmail.com>
2021-09-07 19:29:52 -0300

    Finalizando o crud de movie

--- src/controller/movieController.js
+++ src/controller/movieController.js
@@ -1,7 +1,7 @@
-import { searchAll, addMovie, getMovie } from '../model/movieModel';
+import { add, list, get, remove, update } from '../model/movieModel';
 
-export function add(req, res) {
-  addMovie(req.body.name, req.body.director, req.body.year, req.body.genre_id).then((response) => {
+export function addMovie(req, res) {
+  add(req.body.name, req.body.director, req.body.year, req.body.genre_id).then((response) => {
     return res.status(200).send({ message: 'Filme adicionado com sucesso! :)' });
   }).catch((error) => {
     return res.status(500).send({ message: 'Erro ao adicionar filme! :(', error })
@@ -9,7 +9,7 @@ export function add(req, res) {
 }
 
 export function findAll(req, res) {
-  searchAll().then((response) => {
+  list().then((response) => {
     return res.status(200).send(response);
   }).catch((error) => {
     return res.status(404);
@@ -17,8 +17,25 @@ export function findAll(req, res) {
 }
 
 export function getMovieById(req, res) {
-  getMovie(req.params.id).then((response) => {
+  get(req.params.id).then((response) => {
     return res.status(200).send(response);
+  }).catch((error) => {
+    return res.status(404);
+  })
+}
+
+export function updateMovie(req, res) {
+  update(req.params.id, req.body.name, req.body.director, req.body.year, req.body.genre_id)
+  .then((response) => {
+    return res.status(200).send({ message: 'Filme atualizado com sucesso! :)'});
+  }).catch((error) => {
+    return res.status(404).send({ message: 'Erro ao tentar atualizar o filme! :(', error });
+  })
+}
+
+export function deleteMovie(req, res) {
+  remove(req.params.id).then((response) => {
+    return res.status(200).send({ message: 'Filme removido' });
   }).catch((error) => {
     return res.status(404);
   })
